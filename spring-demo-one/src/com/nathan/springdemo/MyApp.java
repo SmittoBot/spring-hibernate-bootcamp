75573b66c43fc2e1f9e5797518c478cd897bcbe7
Nathan Smith <nathansforyou@gmail.com>
2019-01-31 23:38:34 -0500

    Added inversion of control, applicationcontext and bean creation

--- spring-demo-one/src/com/nathan/springdemo/MyApp.java
+++ spring-demo-one/src/com/nathan/springdemo/MyApp.java
@@ -4,9 +4,9 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// create the object
-		Coach coach = new TrackCoach();
+		Coach myCoach = new TrackCoach();
 		// use the object
-		System.out.println(coach.getDailyWorkout());
+		System.out.println(myCoach.getDailyWorkout());
 	}
 
 }
